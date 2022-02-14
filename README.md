f09a54476d5c43092fe2b52d9fd5e4517d14d68e
ajibade3210 <ajibade69th@gmail.com>
2022-02-14 09:09:31 +0100

    adding first commit

---
+++ README.md
@@ -0,0 +1,30 @@
+# Using Typescript with React Demos
+
+TypeScript can catch bugs when we make mistakes in our code. That’s great, but TypeScript can also prevent us from making those mistakes in the first place.
+
+The type-checker has information to check things like whether we’re accessing the right properties on variables and other properties. Once it has that information, it can also start suggesting which properties you might want to use.
+
+That means TypeScript can be leveraged for editing code too, and the core type-checker can provide error messages and code completion as you type in the editor. That’s part of what people often refer to when they talk about tooling in TypeScript.
+
+In the project directory, you can run:
+
+### `npm start`
+
+Runs the app in the development mode.\
+Open [http://localhost:3000](http://localhost:3000) to view it in the browser.
+
+The page will reload if you make edits.\
+You will also see any lint errors in the console.
+
+### `npm test`
+
+Launches the test runner in the interactive watch mode.\
+See the section about [running tests](https://facebook.github.io/create-react-app/docs/running-tests) for more information.
+
+### `npm run build`
+
+Builds the app for production to the `build` folder.\
+It correctly bundles React in production mode and optimizes the build for the best performance.
+
+The build is minified and the filenames include the hashes.\
+Your app is ready to be deployed!
---
+++ src/components/SingleTodo.tsx
@@ -0,0 +1,105 @@
+import React, { useEffect, useRef, useState } from 'react'
+import { Todo } from './model'
+import {AiFillEdit, AiFillDelete} from "react-icons/ai"
+import {MdDone} from "react-icons/md"
+import "./styles.css"
+import { Draggable } from 'react-beautiful-dnd'
+
+// interface Props {
+//     todo: Todo;
+//     todos: Todo[];
+//     setTodos: React.Dispatch<React.SetStateAction<Todo[]>>;
+// }
+
+type Props = {
+    todo: Todo,
+    todos: Todo[];
+    setTodos: React.Dispatch<React.SetStateAction<Todo[]>>;
+    index: number;
+}
+
+const SingleTodo: React.FC<Props> = ({todo, todos, setTodos, index}) => {
+const [edit, setEdit] = useState<boolean>(false)
+const [editTodo, setEditTodo] = useState<string>(todo.todo)
+
+
+  const handleDone = (id: number) => {
+  setTodos(todos.map((todo)=>
+    todo.id ===id ? {...todo, isDone : !todo.isDone} : todo ))
+}
+
+const handleDelete = (id: number) => {
+  setTodos(todos.filter((todo)=> todo.id !== id))
+}
+
+const handleEditMode = () => {
+  if(!edit && !todo.isDone) {
+    setEdit(!edit)
+  }
+}
+
+const handleEdit = (e: React.FormEvent, id: number) => {
+  e.preventDefault()
+  setTodos(
+    todos.map((todo) => (todo.id === id ? {...todo, todo: editTodo} : todo))
+  )
+  setEdit(false)
+}
+
+const inputRef = useRef<HTMLInputElement>(null)
+
+useEffect(() => {
+  inputRef.current?.focus()
+},[edit])
+
+
+  return (
+    <Draggable draggableId={todo.id.toString()} index={index}>
+      {(provided, snapshot) => (
+    <form
+    className={`todos__single ${snapshot.isDragging ? "drag" : ""}`}
+    onSubmit={(e: React.FormEvent)=>handleEdit(e,todo.id)}
+    {...provided.draggableProps}
+    {...provided.dragHandleProps}
+    ref={provided.innerRef}
+    >
+      {
+        edit ? (
+          <input
+          ref={inputRef}
+          value={editTodo}
+          onChange={(e)=> setEditTodo(e.target.value)}
+          className="todos__single--text"
+           />
+        ) : (
+          todo.isDone ? (
+           <s className="todos__single--text">{todo.todo}</s>
+         ) : (
+           <span className="todos__single--text">{todo.todo}</span>
+         )
+        )
+      }
+
+
+        <div>
+          <span className="icon" onClick={()=> handleEditMode()}>
+            <AiFillEdit />
+          </span>
+          <span className="icon" onClick={()=> handleDelete(todo.id)}>
+          <AiFillDelete/>
+          </span>
+          <span className="icon" onClick={()=> handleDone(todo.id)}>
+          <MdDone />
+          </span>
+        </div>
+    </form>
+      )
+
+      }
+
+
+    </Draggable>
+  )
+}
+
+export default SingleTodo
